d4c81faca098cbabba9d9acf4a003ec9aec5e450
botarokubo <azimazan98@gmail.com>
2026-03-10 04:18:07 +0800

    new route update

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken"); //generate token for login
 const User = require("../models/User");
 
 // When user register, waiting for approval (PENDING -> JVCNUMBER)
@@ -26,7 +27,7 @@ router.post("/register", async (req, res) => {
         email,
         passwordHash,
         phoneNumber,
-        ststus : "PENDING",
+        status : "PENDING",
         band : "MEMBER",
         jvcNumber : null,
     });
@@ -41,4 +42,57 @@ router.post("/register", async (req, res) => {
     }
 });
 
+// For login route
+router.post("/login", async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        if(!email){
+            return res.status(400).json({message: "Email Required"});
+        }else if (!password){
+            return res.status(400).json({message: "Password Required"});
+        }
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message: "Invalid Email or Password"});
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+        if(!passwordMatch){
+            return res.status(401).json({message: "Invalid email or Password"});
+        }
+
+        if (user.status !== "APPROVED"){
+            return res.status(403).json({message: "Invalid email or Password"});
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                email: user.email,
+                band: user.band,
+                jvcNumber: user.jvcNumber,
+            },
+            process.env.JWT_SECRET,
+            {expiresIn: "7d"}
+            );
+
+        return res.status(200).json({
+            message: "Login Successful",
+            token, 
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                phoneNumber: user.phoneNumber,
+                status: user.status,
+                band: user.band,
+                jvcNumber: user.jvcNumber,
+            },
+        });
+    } catch (err) {
+        return res.status(500).json({message: err.message});
+    }
+});
 module.exports = router;
